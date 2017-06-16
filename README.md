6e27e4e827a5f3283224bd950a0e248e33d3aa98
liyonlgin <liyonglin0704@163.com>
2017-06-16 23:29:44 +0800

    first code commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+#  Android Demostrations
+## The most common grammar tests 
+
 this project  record the most common android grammar demostrations ,
 which can help you save a lot of personal time  to check weather the specific function work.
 
--- app/src/main/java/com/android/lee/MainActivity.java
+++ app/src/main/java/com/android/lee/MainActivity.java
@@ -2,6 +2,8 @@ package com.android.lee;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Process;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("***** Process ID", Process.myPid() + "");
+
     }
 }
