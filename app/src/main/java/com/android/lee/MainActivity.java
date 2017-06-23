afef1bcbcff4d8b0426f5bfacfce8427d68ca997
liyonglin <liyonglin0704@163.com>
2017-06-23 09:31:12 +0800

    Serivce Test

--- app/src/main/java/com/android/lee/MainActivity.java
+++ app/src/main/java/com/android/lee/MainActivity.java
@@ -1,18 +1,57 @@
 package com.android.lee;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.Process;
+import android.os.IBinder;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.android.lee.service.IMService;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            Log.e("onServiceConnected", " ***");
+            IMService.MyBinder myBinder = (IMService.MyBinder) iBinder;
+            Toast.makeText(MainActivity.this, "服务端整数：" + myBinder.getService().getRandomNum() + "", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            Log.e("onServiceDisconnected", " ***");
+            Toast.makeText(MainActivity.this,  "onServiceDisconnected" , Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("***** Process ID", Process.myPid() + "");
+        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+    }
 
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                Intent serviceIntent = new Intent(this, IMService.class);
+                bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE);
+                break;
+
+            case R.id.button2:
+                unbindService(connection);
+                break;
+
+        }
     }
 }
